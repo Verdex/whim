72ce0beb0ec6a8445dc578c5c75419a0d51d4638
David L. Inman <davidlevere@gmail.com>
2022-09-26 22:09:37 -0400

    random trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+
+trait Random { 
+    type T;
+
+    fn next(&mut self) -> Self::T;
+    fn range(&mut self, start : Self::T, end : Self::T) -> Self::T;
+    fn ratio(&mut self) -> f64;
 }
 
 #[cfg(test)]

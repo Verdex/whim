63c1fc681e6be5d50ccfc1da587d68df4f7786d5
David L. Inman <davidlevere@gmail.com>
2022-09-26 22:54:21 -0400

    Pcg32Shift; constants

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,16 @@ trait Random {
     fn ratio(&mut self) -> f64;
 }
 
-struct Lcg60 {
+const INIT : u64 = 0xFA_07_45_7E_55_76_FD_81;
+const A : u64 = 0x07_5A_A3_42_BB_49_C9_AD;
+
+pub struct Lcg60 {
     state : u64
 }
 
 impl Lcg60 {
     pub fn new() -> Self {
-        Lcg60 { state: 0xFA_07_45_7E_55_76_FD_81 }
+        Lcg60 { state: INIT }
     }
     pub fn seed(input : u64) -> Self {
         Lcg60 { state : input | 1 }
@@ -23,7 +26,6 @@ impl Lcg60 {
 impl Random for Lcg60 {
     type T = u64;
     fn next(&mut self) -> Self::T {
-        const A : u64 = 0x07_5A_A3_42_BB_49_C9_AD;
         self.state = self.state.wrapping_mul(A);
         return self.state >> 4;
     }
@@ -38,6 +40,37 @@ impl Random for Lcg60 {
     }
 } 
 
+pub struct Pcg32Shift {
+    state : u64
+}
+
+impl Pcg32Shift {
+    pub fn new() -> Self {
+        Pcg32Shift { state: INIT }
+    }
+    pub fn seed(input : u64) -> Self {
+        Pcg32Shift { state : input | 1 }
+    }
+}
+
+impl Random for Pcg32Shift {
+    type T = u32;
+    fn next(&mut self) -> Self::T {
+        self.state = self.state.wrapping_mul(A);
+        let shift = (self.state >> 60) as u8;
+        println!("{}", 60 - (shift + 13));
+        return (self.state >> (shift + 13)) as u32;
+    }
+    fn range(&mut self, start : Self::T, end : Self::T) -> Self::T {
+        (self.ratio() * (end + 1 - start) as f64 + start as f64).floor() as Self::T
+    }
+    fn ratio(&mut self) -> f64 {
+        let a = self.next();
+        let b = self.next();
+        if a < b { a as f64 / b as f64 }
+        else { b as f64 / a as f64 }
+    }
+} 
 
 #[cfg(test)]
 mod tests {

16a73bb895fc79ec82702b23b5ca5f89ff425e52
David L. Inman <davidlevere@gmail.com>
2022-09-26 22:10:03 -0400

    Lcg60 impl

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,38 @@ trait Random {
     fn ratio(&mut self) -> f64;
 }
 
+struct Lcg60 {
+    state : u64
+}
+
+impl Lcg60 {
+    pub fn new() -> Self {
+        Lcg60 { state: 0xFA_07_45_7E_55_76_FD_81 }
+    }
+    pub fn seed(input : u64) -> Self {
+        Lcg60 { state : input | 1 }
+    }
+}
+
+impl Random for Lcg60 {
+    type T = u64;
+    fn next(&mut self) -> Self::T {
+        const A : u64 = 0x07_5A_A3_42_BB_49_C9_AD;
+        self.state = self.state.wrapping_mul(A);
+        return self.state >> 4;
+    }
+    fn range(&mut self, start : Self::T, end : Self::T) -> Self::T {
+        (self.ratio() * (end + 1 - start) as f64 + start as f64).floor() as Self::T
+    }
+    fn ratio(&mut self) -> f64 {
+        let a = self.next();
+        let b = self.next();
+        if a < b { a as f64 / b as f64 }
+        else { b as f64 / a as f64 }
+    }
+} 
+
+
 #[cfg(test)]
 mod tests {
     use super::*;

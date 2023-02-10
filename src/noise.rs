94a77f029b10e736282abec666846b3c3eb34753
David L. Inman <davidlevere@gmail.com>
2023-02-09 23:41:17 -0500

    probably most of noise u32

--- src/noise.rs
+++ src/noise.rs
@@ -1,7 +1,16 @@
 
-pub struct Noise32(pub (crate) u32);
+pub struct NoiseU32(pub (crate) u32);
 
-pub trait NoiseFunc32 {
-    fn noise(&self, input : &[u32]) -> Noise32;
-    fn s_noise(&self, input : &[u32], seed : u32) -> Noise32;
+impl NoiseU32 {
+    pub fn ratio(&self) -> f64 {
+        self.0 as f64 / u32::MAX as f64
+    }
+    pub fn value(&self) -> u32 {
+        self.0
+    }
+}
+
+pub trait NoiseFuncU32 {
+    fn noise(&self, input : &[u32]) -> NoiseU32;
+    fn s_noise(&self, input : &[u32], seed : u32) -> NoiseU32;
 }
--- src/noise_algo.rs
+++ src/noise_algo.rs
@@ -1,97 +1,115 @@
 
 use crate::noise::*;
 
-pub struct CCNoiseFunc32(); 
+pub struct CCNoiseFuncU32(); 
+
+impl CCNoiseFuncU32 {
+    pub fn new() -> Self {
+        CCNoiseFuncU32()
+    }
+}
 
 const Y_PRIME : u32 = 0x6AC2D;
 const Z_PRIME : u32 = 0x41ED2;
 const T_PRIME : u32 = 0xCD67;
 
-// 11110010101011101111101000010111:4071553559:F2AEFA17
-// 11110000100011011000100001010111:4035807319:F08D8857
-// 00110101101000111011000011000101:899920069:35A3B0C5
 
-// 00011110000100011100110001010011:504482899:1E11CC53
-// 00000110111010111010100011011111:116107487:6EBA8DF
-// 00010101100001101011101101110011:361151347:1586BB73
+const PRIME_1 : u32 = 0xF2AEFA17;
+const PRIME_2 : u32 = 0xF08D8857;
+const PRIME_3 : u32 = 0x35A3B0C5;
+const PRIME_4 : u32 = 0x1E11CC53;
+const PRIME_5 : u32 = 0x6EBA8DF;
+const PRIME_6 : u32 = 0x1586BB73;
+const PRIME_7 : u32 = 0xB5D8B5A5;
+const PRIME_8 : u32 = 0xAC5B5253;
+const PRIME_9 : u32 = 0xBE648801;
+const _PRIME_A : u32 = 0x9920A971;
+const _PRIME_B : u32 = 0x1999C52F;
 
-// 10110101110110001011010110100101:3050878373:B5D8B5A5
-// 10101100010110110101001001010011:2891666003:AC5B5253
-// 10111110011001001000100000000001:3194259457:BE648801
-// 10011001001000001010100101110001:2569054577:9920A971
+fn cc_noise(mut input : u32, seed : u32) -> u32 {
 
+    input = input.wrapping_mul(PRIME_1);
+    input = input.wrapping_add(seed);
+    input = input.wrapping_add(PRIME_2);
+    input = input.wrapping_mul(PRIME_3);
+    input = input.wrapping_add(PRIME_4 * ((input >> 3) & 1));
+    input = input.wrapping_mul(PRIME_5);
 
-// 00011001100110011100010100101111:429507887:1999C52F
+    input = input.wrapping_add(PRIME_6 * ((input >> 11) & 1));
+    input = input.wrapping_mul(PRIME_7);
+    
+    input = input.wrapping_mul(input);
 
+    let shift = (input & 0xF) + 3;
 
-fn cc_noise(mut x : u32, seed : u32) -> u32 {
-    x = x.wrapping_add(11);
-    x = x.wrapping_mul(17);
-    x = x.wrapping_mul(x);
-    x ^= x >> 3;
-    x
+    input ^= input >> shift;
+
+    input = input.wrapping_add(PRIME_8 * ((input >> 23) & 1));
+    input = input.wrapping_mul(PRIME_9);
+
+    input ^= input >> 17;
+
+    input 
 }
 
-impl NoiseFunc32 for CCNoiseFunc32 {
-    fn noise(&self, input : &[u32]) -> Noise32 {
+impl NoiseFuncU32 for CCNoiseFuncU32 {
+    fn noise(&self, input : &[u32]) -> NoiseU32 {
         match input {
             [x, y, z, t] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
                              .wrapping_add(z.wrapping_mul(Z_PRIME))
                              .wrapping_add(t.wrapping_mul(T_PRIME));
                 let n = cc_noise(input, 0);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x, y, z] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
                              .wrapping_add(z.wrapping_mul(Z_PRIME));
                 let n = cc_noise(input, 0);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x, y] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME));
                 let n = cc_noise(input, 0);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x] => {
                 let n = cc_noise(*x, 0);
-                Noise32(n)
+                NoiseU32(n)
             },
-            _ => panic!("CCNoise32 only supports 1-4 D noise"),
+            _ => panic!("CCNoiseU32 only supports 1-4 D noise"),
         }
     }
 
-    fn s_noise(&self, input : &[u32], seed : u32) -> Noise32 {
+    fn s_noise(&self, input : &[u32], seed : u32) -> NoiseU32 {
         match input {
             [x, y, z, t] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
                              .wrapping_add(z.wrapping_mul(Z_PRIME))
                              .wrapping_add(t.wrapping_mul(T_PRIME));
                 let n = cc_noise(input, seed);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x, y, z] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
                              .wrapping_add(z.wrapping_mul(Z_PRIME));
                 let n = cc_noise(input, seed);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x, y] => {
                 let input = x.wrapping_add(y.wrapping_mul(Y_PRIME));
                 let n = cc_noise(input, seed);
-                Noise32(n)
+                NoiseU32(n)
             },
             [x] => {
                 let n = cc_noise(*x, seed);
-                Noise32(n)
+                NoiseU32(n)
             },
             _ => panic!("CCNoise32 only supports 1-4 D noise"),
         }
     }
 }
 
-// seed is probably just add in towards the beginning but not first thing
-// multi param is something like  x + (prime1 * y) + (prime2 * z) ...
 
 // xor (shift dependent amount)
 // special bit indicates mul against prime 

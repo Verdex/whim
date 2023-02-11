5952825a01a86bdb6d29c7d896119cffe129260f
David L. Inman <davidlevere@gmail.com>
2023-02-10 22:46:40 -0500

    inc prog noise direciton change (cc index algo)

--- src/noise.rs
+++ src/noise.rs
@@ -1,34 +1,10 @@
 
-pub struct NoiseU32(pub (crate) u32);
-
-impl NoiseU32 {
-    pub fn ratio(&self) -> f64 {
-        self.0 as f64 / u32::MAX as f64
-    }
-    pub fn value(&self) -> u32 {
-        self.0
-    }
-}
-
-pub struct NoiseF64(pub (crate) u32, pub (crate) u32);
-
-impl NoiseF64 {
-    pub fn ratio(&self) -> f64 {
-        if self.0 > self.1 {
-            self.1 as f64 / self.0 as f64
-        }
-        else {
-            self.0 as f64 / self.1 as f64
-        }
-    }
-}
-
 pub trait NoiseFuncU32 {
-    fn noise(&self, input : &[u32]) -> NoiseU32;
-    fn s_noise(&self, input : &[u32], seed : u32) -> NoiseU32;
+    fn noise(&self, input : &[u32]) -> f32;
+    fn s_noise(&self, input : &[u32], seed : u32) -> f32;
 }
 
-pub trait NoiseFuncF64 {
-    fn noise(&self, input : &[f64]) -> NoiseF64;
-    fn s_noise(&self, input : &[f64], seed : u32) -> NoiseF64;
+pub trait NoiseFuncF32 {
+    fn noise(&self, input : &[f32]) -> f32;
+    fn s_noise(&self, input : &[f32], seed : u32) -> f32;
 }
--- src/noise_algo.rs
+++ src/noise_algo.rs
@@ -1,7 +1,15 @@
 
 use crate::noise::*;
 
-pub struct CCNoiseFuncU32(); 
+pub struct CCIndexU32 { }
+
+impl CCIndexU32 {
+    pub fn new() -> Self {
+        CCIndexU32 { }
+    }
+}
+
+/*pub struct CCNoiseFuncU32(); 
 
 impl CCNoiseFuncU32 {
     pub fn new() -> Self {
@@ -15,12 +23,13 @@ impl CCNoiseFuncF64 {
     pub fn new() -> Self {
         CCNoiseFuncF64()
     }
-}
+}*/
 
 const Y_PRIME : u32 = 0x6AC2D;
 const Z_PRIME : u32 = 0x41ED2;
 const T_PRIME : u32 = 0xCD67;
 
+const DIM_PRIMES : [u32;4] = [1, Y_PRIME, Z_PRIME, T_PRIME];
 
 const PRIME_1 : u32 = 0xF2AEFA17;
 const PRIME_2 : u32 = 0xF08D8857;
@@ -34,7 +43,63 @@ const PRIME_9 : u32 = 0xBE648801;
 const _PRIME_A : u32 = 0x9920A971;
 const _PRIME_B : u32 = 0x1999C52F;
 
-fn cc_noise(mut input : u32, seed : u32) -> u32 {
+fn cc_index_noise(mut input : u32, seed : u32) -> u32 {
+    input = input.wrapping_mul(PRIME_1);
+    input = input.wrapping_add(seed);
+    input = input.wrapping_add(PRIME_2);
+
+    let shift = (input & 0xF) + 1;
+    input ^= input >> shift;
+
+    let shift = ((input >> 4) & 0xF) + 1;
+    input ^= input << shift;
+
+    let shift = ((input >> 28) & 0xF) + 1;
+    input ^= input >> shift;
+
+    let shift = ((input >> 24) & 0xF) + 1;
+    input ^= input << shift;
+
+    input = input.wrapping_mul(input);
+    input = input.wrapping_mul(PRIME_3);
+    input = input.wrapping_add(PRIME_4 * ((input >> 13) & 1));
+    input ^= input >> 17;
+    input = input.wrapping_add(PRIME_5 * ((input >> 19) & 1));
+    input ^= input << 7;
+    input
+}
+
+impl NoiseFuncU32 for CCIndexU32 {
+    fn noise(&self, input : &[u32]) -> f32 {
+        if input.len() > 4 {
+            panic!("noise only supports 1-4 D noise");
+        }
+
+        let input = input.into_iter()
+                         .zip(DIM_PRIMES.into_iter())
+                         .map(|(a, b)| a.wrapping_mul(b))
+                         .fold(0u32, |tot, x| tot.wrapping_add(x));
+
+        let output = cc_index_noise(input, 0);
+        output as f32 / u32::MAX as f32
+    }
+
+    fn s_noise(&self, input : &[u32], seed : u32) -> f32 {
+        if input.len() > 4 {
+            panic!("noise only supports 1-4 D noise");
+        }
+
+        let input = input.into_iter()
+                         .zip(DIM_PRIMES.into_iter())
+                         .map(|(a, b)| a.wrapping_mul(b))
+                         .fold(0u32, |tot, x| tot.wrapping_add(x));
+
+        let output = cc_index_noise(input, seed);
+        output as f32 / u32::MAX as f32
+    }
+}
+
+/*fn cc_noise(mut input : u32, seed : u32) -> u32 {
 
     input = input.wrapping_mul(PRIME_1);
     input = input.wrapping_add(seed);
@@ -58,195 +123,54 @@ fn cc_noise(mut input : u32, seed : u32) -> u32 {
     input ^= input >> 17;
 
     input 
-}
+}*/
 
-impl NoiseFuncU32 for CCNoiseFuncU32 {
-    fn noise(&self, input : &[u32]) -> NoiseU32 {
-        match input {
-            [x, y, z, t] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME))
-                             .wrapping_add(t.wrapping_mul(T_PRIME));
-                let n = cc_noise(input, 0);
-                NoiseU32(n)
-            },
-            [x, y, z] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME));
-                let n = cc_noise(input, 0);
-                NoiseU32(n)
-            },
-            [x, y] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME));
-                let n = cc_noise(input, 0);
-                NoiseU32(n)
-            },
-            [x] => {
-                let n = cc_noise(*x, 0);
-                NoiseU32(n)
-            },
-            _ => panic!("CCNoiseU32 only supports 1-4 D noise"),
+/*impl NoiseFuncU32 for CCNoiseFuncU32 {
+    fn noise(&self, input : &[u32]) -> f32 {
+        if input.len() > 4 {
+            panic!("noise only supports 1-4 D noise");
         }
+
+        let input = input.into_iter()
+                         .zip(DIM_PRIMES.into_iter())
+                         .map(|(a, b)| a.wrapping_mul(b))
+                         .fold(0u32, |tot, x| tot.wrapping_add(x));
+
+        let output = cc_noise(input, 0);
+        output as f32 / u32::MAX as f32
     }
 
-    fn s_noise(&self, input : &[u32], seed : u32) -> NoiseU32 {
-        match input {
-            [x, y, z, t] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME))
-                             .wrapping_add(t.wrapping_mul(T_PRIME));
-                let n = cc_noise(input, seed);
-                NoiseU32(n)
-            },
-            [x, y, z] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME));
-                let n = cc_noise(input, seed);
-                NoiseU32(n)
-            },
-            [x, y] => {
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME));
-                let n = cc_noise(input, seed);
-                NoiseU32(n)
-            },
-            [x] => {
-                let n = cc_noise(*x, seed);
-                NoiseU32(n)
-            },
-            _ => panic!("CCNoiseU32 only supports 1-4 D noise"),
+    fn s_noise(&self, input : &[u32], seed : u32) -> f32 {
+        if input.len() > 4 {
+            panic!("noise only supports 1-4 D noise");
         }
+
+        let input = input.into_iter()
+                         .zip(DIM_PRIMES.into_iter())
+                         .map(|(a, b)| a.wrapping_mul(b))
+                         .fold(0u32, |tot, x| tot.wrapping_add(x));
+
+        let output = cc_noise(input, seed);
+        output as f32 / u32::MAX as f32
     }
 }
+
+
+                let x = x.to_be_bytes();
+                let y = y.to_be_bytes();
+                let z = z.to_be_bytes();
+                let t = t.to_be_bytes();
+
+                let x = u64::from_be_bytes(x);
+                let y = u64::from_be_bytes(y);
+                let z = u64::from_be_bytes(z);
+                let t = u64::from_be_bytes(t);
 
 impl NoiseFuncF64 for CCNoiseFuncF64 {
     fn noise(&self, input : &[f64]) -> NoiseF64 {
-        match input {
-            [x, y, z, t] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-                let z = z.to_be_bytes();
-                let t = t.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-                let z = u64::from_be_bytes(z);
-                let t = u64::from_be_bytes(t);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME.into()))
-                             .wrapping_add(t.wrapping_mul(T_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, 0);
-                let n2 = cc_noise((input >> 32) as u32, 0);
-                NoiseF64(n1, n2)
-            },
-            [x, y, z] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-                let z = z.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-                let z = u64::from_be_bytes(z);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, 0);
-                let n2 = cc_noise((input >> 32) as u32, 0);
-                NoiseF64(n1, n2)
-            },
-            [x, y] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, 0);
-                let n2 = cc_noise((input >> 32) as u32, 0);
-                NoiseF64(n1, n2)
-            },
-            [x] => {
-                let x = x.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-
-                let input = x;
-
-                let n1 = cc_noise(input as u32, 0);
-                let n2 = cc_noise((input >> 32) as u32, 0);
-                NoiseF64(n1, n2)
-            },
-            _ => panic!("CCNoiseF64 only supports 1-4 D noise"),
-        }
     }
 
     fn s_noise(&self, input : &[f64], seed : u32) -> NoiseF64 {
-        match input {
-            [x, y, z, t] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-                let z = z.to_be_bytes();
-                let t = t.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-                let z = u64::from_be_bytes(z);
-                let t = u64::from_be_bytes(t);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME.into()))
-                             .wrapping_add(t.wrapping_mul(T_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, seed);
-                let n2 = cc_noise((input >> 32) as u32, seed);
-                NoiseF64(n1, n2)
-            },
-            [x, y, z] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-                let z = z.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-                let z = u64::from_be_bytes(z);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()))
-                             .wrapping_add(z.wrapping_mul(Z_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, seed);
-                let n2 = cc_noise((input >> 32) as u32, seed);
-                NoiseF64(n1, n2)
-            },
-            [x, y] => {
-                let x = x.to_be_bytes();
-                let y = y.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-                let y = u64::from_be_bytes(y);
-
-                let input = x.wrapping_add(y.wrapping_mul(Y_PRIME.into()));
-
-                let n1 = cc_noise(input as u32, seed);
-                let n2 = cc_noise((input >> 32) as u32, seed);
-                NoiseF64(n1, n2)
-            },
-            [x] => {
-                let x = x.to_be_bytes();
-
-                let x = u64::from_be_bytes(x);
-
-                let input = x;
-
-                let n1 = cc_noise(input as u32, seed);
-                let n2 = cc_noise((input >> 32) as u32, seed);
-                NoiseF64(n1, n2)
-            },
-            _ => panic!("CCNoiseF64 only supports 1-4 D noise"),
-        }
     }
-}
+}*/
 
